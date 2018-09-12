d29a566d1cc3b2bb0c131e8bef67e375d2951f71
Rafael de Freitas <dfr.rafael@gmail.com>
2018-09-12 09:12:57 -0300

    Updating LifeSystem
    Should be working now. (I hope)

--- Assets/Modules/Particles_ECS/Scripts/LifeSystem.cs
+++ Assets/Modules/Particles_ECS/Scripts/LifeSystem.cs
@@ -1,39 +1,44 @@
-﻿//using Unity.Collections;
-//using Unity.Entities;
-//using Unity.Jobs;
-//using Unity.Mathematics;
-//using Unity.Transforms;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 
-//public class LifeSystem : ComponentSystem
-//{
+public class LifeSystem : ComponentSystem
+{
 
-//    //public struct Group
-//    //{
-//    //    [ReadOnly]
-//    //    public ComponentDataArray<Position> Positions;
+    public struct Group
+    {
+        [ReadOnly]
+        public ComponentDataArray<Particles.ParticleLife> ParticlesLife;
 
-//    //    // Sometimes it is necessary to not only access the components
-//    //    // but also the Entity ID.
-//    //    public EntityArray Entities;
+        // Sometimes it is necessary to not only access the components
+        // but also the Entity ID.
+        public EntityArray Entities;
 
-//    //    // The Length can be injected for convenience as well 
-//    //    [ReadOnly] public int Length;
-//    //}
+        // The Length can be injected for convenience as well 
+        public readonly int Length;
+    }
 
-//    //[Inject] private Group m_Group;
+    [Inject] private Group _Group;
 
-//    //protected override void OnUpdate()
-//    //{
-//    //    // Iterate over all entities matching the declared ComponentGroup required types
-//    //    for (int i = 0; i != m_Group.Length; i++)
-//    //    {
-//    //        if(m_Group.Positions[i].Value.y > 5.0f)
-//    //        {
+    protected override void OnUpdate()
+    {
+		
+		float dt = Time.deltaTime;
+		
+        // Iterate over all entities matching the declared ComponentGroup required types
+        for (int i = 0; i != _Group.Length; i++)
+        {
+			
+			_Group.ParticlesLife[i].lifeTime += dt;
+			
+            if(_Group.ParticlesLife[i].lifeTime > _Group.ParticlesLife[i].lifeSpan)
+            {
+                Entity entity = _Group.Entities[i];
+                World.Active.GetExistingManager<EntityManager>().DestroyEntity(entity);
+            }
+        }
+    }
 
-//    //            Entity entity = m_Group.Entities[i];
-//    //            World.Active.GetExistingManager<EntityManager>().DestroyEntity(entity);
-//    //        }
-//    //    }
-//    //}
-
-//}
+}
